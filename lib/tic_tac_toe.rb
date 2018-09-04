d3cc3051f9834ac5163b31c43d719443f5eb90e9
carly <carly@noukod.com>
2018-09-04 17:21:44 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -10,7 +10,7 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def move(board, index, current_player== "X")
+def move(board, index, current_player
   board[index] = current_player
 end
 
